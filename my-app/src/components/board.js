5469ad12390d648d4e3d8604c006b8e2cc954900
MisonoTakezo <misonotakezo1213@gmail.com>
2021-08-22 17:58:06 +0900

    split files with functions

---
+++ my-app/src/components/board.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Square from "./square.js";
+import calculateWinner from "../utils/calcWin.js";
+
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+    setSquares(squares);
+    setXIsNext(!xIsNext);
+  }
+
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner:" + winner;
+  } else {
+    status = "Next player:" + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+export default Board;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,79 +1,10 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import calculateWinner from "./utils/calcWin.js";
+import Game from "./components/game.js"
 
-const Square = ({ value, onClick }) => {
-
+const App = () => {
   return (
-    <button className="square" onClick={() => onClick()}>
-      {value}
-    </button>
+    <Game />
   );
 }
-
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const handleClick = (i) => {
-
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = xIsNext ? "X" : "O";
-    setSquares(squares);
-    setXIsNext(!xIsNext);
-  }
-
-  const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
-  };
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner:" + winner;
-  } else {
-    status = "Next player:" + (xIsNext ? "X" : "O");
-  }
-
-  return (
-    <div>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  );
-};
-
-const Game = () => {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
-};
-
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
